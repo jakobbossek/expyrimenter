fc31e95ffcc4a9a9682cdeecf598b490a3925664
Jakob Bossek <j.bossek@gmail.com>
2026-07-15 14:47:06 +0200

    update github workflow

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     url = 'https://github.com/jakobbossek/expyrimenter/',
     # tell PyPi that we use markdown and not RestructuredText
     packages = setuptools.find_packages(exclude = ['tests']),
-    python_requires = '>=3.7',
+    python_requires = '>=3.12',
     # py_modules = ['randvec'],
     # package_dir = {},
     # make it easy for users to find the package
@@ -17,12 +17,7 @@ setuptools.setup(
     classifiers = [
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         # 'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
